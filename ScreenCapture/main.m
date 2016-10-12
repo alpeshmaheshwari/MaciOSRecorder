0e5f2e4d0071b4454b73dfff5772d8a5d68978de
Kobiton DevOps <kobiton-devops@kobitons-imac.local>
2016-10-12 10:00:09 +0700

    Initial Commit

---
+++ ScreenCapture/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ScreenCapture
+//
+//  Created by Kobiton DevOps on 10/12/16.
+//  Copyright © 2016 Kobiton DevOps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
